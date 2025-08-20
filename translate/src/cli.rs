7ca0ad8724a7a2e76aba92d07f9711f1ca95572a
Johnathan Van Why <jrvanwhy@betterbytes.org>
2025-08-20 14:47:37 -0700

    Review comments: `cargo test` and `--in_performer` docs.

--- translate/src/cli.rs
+++ translate/src/cli.rs
@@ -9,7 +9,8 @@ pub struct Args {
     // control history, code review comments, etc). When that format has been
     // defined, we'll add a separate flag to specify it, and change the
     // requirement to "pass either --in_performer or the other input flag".
-    /// Path to an input project in the TRACTOR performer format.
+    /// Path to the C code to translate. This path should be a directory in the
+    /// project structure defined by the TRACTOR_Performers library.
     #[arg(long)]
     in_performer: PathBuf,
 }
