7276679e167cbf496d2cdb223ceb79bb205f8729
Johnathan Van Why <jrvanwhy@betterbytes.org>
2025-08-26 17:25:36 -0700

    Tool trait: choose the "read snapshot, no conflicting writes" concurrency model.
    Before running a tool, the scheduler will invoke `Tool::may_write` to learn which IR IDs the tool will write. The scheduler uses this to avoid running the tool if it will edit the same ID as another concurrent tool invocation. `may_write` has read-only access to a snapshot of the IR which it can scan through to find representations it is interested in.
    
    If the tool is ran, it is given a `context` object that gives it read-only access to the same snapshot as `may_write` saw as well as write access to the IDs that `may_write` returned. The tool invocation can also create new IDs and write to those representations, as they are guarantee to not conflict with other tool invocations.
    
    `context`'s interface is not yet implemented, because implementing it will require making a number of changes to the IR that will conflict with another PR.

--- translate/src/tool.rs
+++ translate/src/tool.rs
@@ -1,4 +1,5 @@
-use harvest_ir::{Id, Representation};
+use harvest_ir::{HarvestIR, Id};
+use std::sync::Arc;
 
 /// Trait implemented by each tool. Used by the scheduler to decide what tools
 /// to run and to manage those tools.
@@ -6,7 +7,7 @@ use harvest_ir::{Id, Representation};
 /// An instance of Tool represents a particular invocation of that tool (i.e.
 /// certain arguments and a certain initial IR state). The scheduler constructs
 /// a Tool when it is considering running that tool, and then decides whether to
-/// invoke the tool based on which portions of the IR it uses.
+/// invoke the tool based on which parts of the IR it writes.
 ///
 /// The tool's constructor does not appear in the Tool trait, because at the
 /// time the scheduler constructs the tool it is aware of the tool's concrete
@@ -15,38 +16,43 @@ use harvest_ir::{Id, Representation};
 /// chooses to invoke. Tool is also intentionally dyn compatible.
 #[allow(dead_code)] // Remove when scheduler implemented.
 pub trait Tool: Send {
-    /// Indicate which portion of the IR this tool will access. This is dynamic
-    /// and not `const` because it can depend on the arguments the tool was
-    /// constructed with.
-    fn access_required(&self) -> IRAccess;
+    /// Returns the IDs this tool may write, or `None` if it is unable to run on
+    /// on this PR.
+    ///
+    /// The IDs returned may depend on the tool constructor's arguments as well
+    /// as the contents of `ir`. Reasons may_write might return `None` include
+    /// but are not limited to:
+    /// 1. The tool requires input data that `ir` does not have.
+    /// 2. The tool creates data that already exists in `ir` so there is nothing
+    ///    to do.
+    fn may_write(&self, ir: &HarvestIR) -> Option<Vec<Id>>;
 
-    /// Runs the tool. The portions of the IR that this tool needs (as it
-    /// requested via `access_required`) are passed in as arguments. If that ID
-    /// does not exist yet, it is passed as `None`.
-    // Note: This is just a conceptual prototype. In practice, these will
-    // probably need to be a runtime-tracked borrow because there's no correct
-    // lifetime to put here.
-    fn run(
-        &self,
-        reads: &[(Id, Option<&Representation>)],
-        writes: &[(Id, &mut Option<Representation>)],
-    );
+    /// Runs the tool logic. IR access and edits are made using `context`.
+    ///
+    /// If `Ok` is returned the changes will be applied to the IR, and if `Err`
+    /// is returned the changes will not be applied.
+    fn run(&self, context: Context) -> Result<(), Box<dyn std::error::Error>>;
 }
 
-/// Represents what portion of the IR a tool accesses.
-pub struct IRAccess {
-    /// The portion of the IR that this tool invocation might read. Note that
-    /// these do not all need to exist; merely checking if a particular
-    /// representation exists is a read operation.
-    #[allow(dead_code)] // Remove when scheduler implemented.
-    reads: Vec<harvest_ir::Id>,
-
-    /// Which part of the IR this tool invocation might write. Note that both
-    /// creating a representation and modifying an existing representation count
-    /// as a write.
-    #[allow(dead_code)] // Remove when scheduler implemented.
-    writes: Vec<harvest_ir::Id>,
+/// Context a tool is provided when it is running. The tool uses this context to
+/// access the IR, make IR changes, launch external processes (with
+/// diagnostics), and anything else that requires hooking into the rest of
+/// harvest_translate.
+#[non_exhaustive]
+pub struct Context {
+    // IR this tool was launched with.
+    _ir_snapshot: Arc<HarvestIR>,
 }
+
+// TODO: Add methods to Context for:
+//   Reading representations from the IR
+//   Getting write access to the IR
+//   Allocating new IDs
+//   Adding a new representation to the IR (combines allocating an ID with getting
+//   write access to it).
+// There are a few nonobvious things about this: how to handle requesting
+// read-only access if the ID is writeable by this Tool, write access for
+// newly-allocated IDs.
 
 #[cfg(test)]
 mod tests {
