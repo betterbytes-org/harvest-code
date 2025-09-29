e4efa10fb371d6bbce14721fcb9e4720f094e75e
Evan Johnson <e5johnso@ucsd.edu>
2025-09-29 08:46:01 -0700

    initial commit as I hack on data structure selection

---
+++ struct_selection/src/main.rs
@@ -0,0 +1,15 @@
+
+/// Some simple examples of abstract operations
+/// Should be sufficient to define Stack, Queues, and Dequeues
+enum AbstractOp{
+    PushFront,
+    PushBack, 
+    PopFront, 
+    PopBack,
+    Len,
+}
+
+
+fn main() {
+    println!("Let's select some structs!");
+}
