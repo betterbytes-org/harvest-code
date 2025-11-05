8e63ce05f57b1d4dbd4d5b1520a6f544100bc363
Evan Johnson <e5johnso@ucsd.edu>
2025-11-05 13:32:36 -0800

    make harvest_translate into a library

--- translate/src/main.rs
+++ translate/src/main.rs
@@ -1,22 +1,8 @@
-mod cli;
-mod runner;
-mod scheduler;
-mod tools;
-
-#[cfg(test)]
-mod test_util;
-
 use clap::Parser;
-use harvest_ir::edit::{self, NewEditError};
-use log::{debug, error, info};
-use runner::{SpawnToolError, ToolRunner};
-use scheduler::Scheduler;
+use harvest_translate::cli::{Args, initialize};
+use harvest_translate::transpile;
+use log::{error, info};
 use std::sync::Arc;
-use tools::MightWriteContext;
-use tools::MightWriteOutcome;
-use tools::load_raw_source::LoadRawSource;
-use tools::raw_source_to_cargo_llm::RawSourceToCargoLlm;
-use tools::try_cargo_build::TryCargoBuild;
 
 fn main() {
     if let Err(e) = run() {
@@ -27,69 +13,11 @@ fn main() {
 
 fn run() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
-    let args: Arc<_> = cli::Args::parse().into();
-    let Some(config) = cli::initialize(args) else {
+    let args: Arc<_> = Args::parse().into();
+    let Some(config) = initialize(args) else {
         return Ok(()); // An early-exit argument was passed.
     };
-    let mut ir_organizer = edit::Organizer::default();
-    let mut runner = ToolRunner::default();
-    let mut scheduler = Scheduler::default();
-    scheduler.queue_invocation(LoadRawSource::new(&config.input.clone()));
-    scheduler.queue_invocation(RawSourceToCargoLlm);
-    scheduler.queue_invocation(TryCargoBuild);
-    loop {
-        let snapshot = ir_organizer.snapshot();
-        scheduler.next_invocations(|mut tool| {
-            let name = tool.name();
-            let might_write = match tool.might_write(MightWriteContext { ir: &snapshot }) {
-                MightWriteOutcome::NotRunnable => {
-                    debug!("Tool {name} is not runnable");
-                    return None;
-                }
-                MightWriteOutcome::Runnable(might_write) => {
-                    debug!("Tool {name} is runnable");
-                    might_write
-                }
-                MightWriteOutcome::TryAgain => {
-                    debug!("Tool {name} returned TryAgain");
-                    return Some(tool);
-                }
-            };
-            match runner.spawn_tool(
-                &mut ir_organizer,
-                tool,
-                snapshot.clone(),
-                might_write,
-                config.clone(),
-            ) {
-                Err(SpawnToolError {
-                    cause: NewEditError::IdInUse,
-                    tool,
-                }) => {
-                    debug!("Not spawning {name} because an ID it needs is in use.");
-                    Some(tool)
-                }
-                Err(SpawnToolError {
-                    cause: NewEditError::UnknownId,
-                    tool: _,
-                }) => {
-                    error!("Tool {name}: might_write returned an unknown ID");
-                    None
-                }
-                Ok(()) => {
-                    info!("Launched tool {name}");
-                    None
-                }
-            }
-        });
-        if !runner.process_tool_results(&mut ir_organizer) {
-            // No tools are running now, which also indicates that no tools are schedulable.
-            // Eventually we need some way to determine whether this is a successful outcome or a
-            // failure, but for now we can just assume success.
-            break;
-        }
-    }
-    let ir = ir_organizer.snapshot();
+    let ir = transpile(config)?;
     info!("{}", ir);
     Ok(())
 }
